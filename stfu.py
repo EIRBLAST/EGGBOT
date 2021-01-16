476f810b4bd77558efc78102b12c4bae67f8742b
EIRBLAST <64648494+EIRBLAST@users.noreply.github.com>
2021-01-16 22:32:41 +0100

    2.0

--- stfu.py
+++ stfu.py
@@ -17,23 +17,15 @@ async def on_ready():
 
 @client.command()
 async def guess(ctx,message):
-    if message.lower() == "storage-website-gacha.herokuapp.com/egg.html":
+    if message.lower() == "CAT":
         await ctx.author.send(embed=discord.Embed(title="You got one thing"))
-    elif message.lower() == "stfurequiemisthebestbot":
-        embed = discord.Embed()
-        embed.set_image(url='http://storage-website-gacha.herokuapp.com/Image/enigma.png')
-        await ctx.author.send(embed=embed)
-    elif message.lower() == "stfuplease!":
-        await ctx.author.send(embed=discord.Embed(title="Search in summons you shall find the answer"))
-    elif message.lower() == "whatnow":
-        await ctx.author.send(embed=discord.Embed(title="https://discord.gg/HGnPJH4"))
     else:
         await ctx.author.send(embed=discord.Embed(title="Try agin !"))
     await ctx.message.delete()
 
 @client.command()
 async def code(ctx,message):
-    if message.lower() == "13491014171":
+    if message.lower() == "LE CHAAT-T":
         await ctx.author.send(embed=discord.Embed(title="DM EIRBLAST THIS CODE GG !"))
     else:
         await ctx.author.send(embed=discord.Embed(title="Try agin !"))
