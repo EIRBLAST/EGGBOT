857d3d286538e103396e0ac4e6790ea8c5d8698d
EIRBLAST <64648494+EIRBLAST@users.noreply.github.com>
2021-01-17 15:09:10 +0100

    Update stfu.py

--- stfu.py
+++ stfu.py
@@ -18,7 +18,7 @@ async def on_ready():
 @client.command()
 async def guess(ctx,message):
     if message.lower() == "CAT":
-        await ctx.author.send(embed=discord.Embed(title="You got one thing"))
+        await ctx.author.send(embed=discord.Embed(title="https://discord.gg/6K4t5qxm7W"))
     else:
         await ctx.author.send(embed=discord.Embed(title="Try agin !"))
     await ctx.message.delete()
